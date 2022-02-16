169cca2722da2cd6601dabb5894b5d2ddc94251c
Javier <javier.bryan@speurgroup.com>
2022-02-15 22:48:29 -0500

    added minutes offset

--- src/main/java/com/example/javeke/controllers/WSController.java
+++ src/main/java/com/example/javeke/controllers/WSController.java
@@ -1,7 +1,9 @@
 package com.example.javeke.controllers;
 
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.ZoneId;
@@ -13,8 +15,8 @@ import java.util.Date;
 @RequestMapping("date")
 public class WSController {
     @GetMapping
-    public String getDate(){
-        Date now = new Date();
+    public String getDate( @RequestParam(name = "minutesOffset", required = false) Integer minutesOffset){
+        Date now = new Date(System.currentTimeMillis() + (1000L * 60 * (minutesOffset == null ? 0 : minutesOffset)));
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("eee MMM d HH:mm:ss ZZ yyyy");
 
         ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(now.toInstant(), ZoneId.systemDefault());
