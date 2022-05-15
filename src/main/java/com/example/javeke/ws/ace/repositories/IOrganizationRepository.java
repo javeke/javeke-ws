545dada09352e8e6bc851e04dfdea3d3318e7ab8
Javier <javier.bryan@speurgroup.com>
2022-05-15 01:22:05 -0500

    updated logging and organization service to fix bug

--- src/main/java/com/example/javeke/ws/ace/repositories/IOrganizationRepository.java
+++ src/main/java/com/example/javeke/ws/ace/repositories/IOrganizationRepository.java
@@ -1,22 +1,12 @@
 package com.example.javeke.ws.ace.repositories;
 
 import com.example.javeke.ws.ace.models.dao.Organization;
-<<<<<<< HEAD
-import org.springframework.data.mongodb.repository.DeleteQuery;
-=======
->>>>>>> d0ad902 (implement websocket API and REST API for the organization CRUD operations)
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IOrganizationRepository extends MongoRepository<Organization, String> {
-<<<<<<< HEAD
-    Organization findByName(String name);
     Organization findByOrganizationId(String organizationId);
     int deleteByOrganizationId(String organizationId);
-=======
 
-    Organization findByName(String name);
-    Organization findByOrganizationId(String organizationId);
->>>>>>> d0ad902 (implement websocket API and REST API for the organization CRUD operations)
 }
