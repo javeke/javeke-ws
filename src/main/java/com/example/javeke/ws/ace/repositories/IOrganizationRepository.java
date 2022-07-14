8984fe4578bc18ff9b05c4c7ae6c474e26333814
Javier <javier.bryan@speurgroup.com>
2022-07-14 00:52:44 -0500

    refactor project structure

--- src/main/java/com/example/javeke/ws/ace/repositories/IOrganizationRepository.java
+++ src/main/java/com/example/javeke/ws/ace/repositories/IOrganizationRepository.java
@@ -1,7 +1,6 @@
 package com.example.javeke.ws.ace.repositories;
 
 import com.example.javeke.ws.ace.models.dao.Organization;
-import org.springframework.data.mongodb.repository.DeleteQuery;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
